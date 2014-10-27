b0e989a35bdf5788f9e3a63774d505eed3d89db5
Pierre Mardon <pierre.mardon@gmail.com>
2014-10-27 16:40:47 +0100

    Added a provider for HS river, refs #7

---
+++ net.funkyjava.gametheory_net.funkyjava.gametheory.gameutil/net.funkyjava.gametheory_net.funkyjava.gametheory.gameutil.poker.he.evaluators/src/main/java/net/funkyjava/gametheory/gameutil/poker/he/evaluators/HSRiverEvaluatorProvider.java
@@ -0,0 +1,39 @@
+package net.funkyjava.gametheory.gameutil.poker.he.evaluators;
+
+import net.funkyjava.gametheory.gameutil.cards.IntCardsSpec;
+import net.funkyjava.gametheory.gameutil.cards.indexing.bucketing.CardsGroupsDoubleEvaluator;
+import net.funkyjava.gametheory.gameutil.cards.indexing.bucketing.CardsGroupsDoubleEvaluatorProvider;
+import net.funkyjava.gametheory.gameutil.poker.he.handeval.Holdem7CardsEvaluatorProvider;
+
+/**
+ * Provider for {@link HSRiverEvaluator}
+ * 
+ * @author Pierre Mardon
+ * 
+ */
+public class HSRiverEvaluatorProvider implements
+		CardsGroupsDoubleEvaluatorProvider {
+
+	private final IntCardsSpec specs;
+	private final Holdem7CardsEvaluatorProvider evalProvider;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param cardsSpec
+	 *            the cards specifications we want the created evaluators to use
+	 * @param evalProvider
+	 *            an holdem evaluator provider
+	 */
+	public HSRiverEvaluatorProvider(IntCardsSpec cardsSpec,
+			Holdem7CardsEvaluatorProvider evalProvider) {
+		this.specs = cardsSpec;
+		this.evalProvider = evalProvider;
+	}
+
+	@Override
+	public CardsGroupsDoubleEvaluator get() {
+		return new HSRiverEvaluator(specs, evalProvider.getEvaluator());
+	}
+
+}
